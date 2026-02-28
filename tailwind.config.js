841ff22196743c31a518e5e51bfcd927907a0dc7
nwtnmusical <nwtnmusical@gmail.com>
2026-02-28 15:22:32 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,10 +15,6 @@ module.exports = {
           secondary: '#cf2100',
         }
       },
-      animation: {
-        'spin-slow': 'spin 3s linear infinite',
-        'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
-      }
     },
   },
   plugins: [],
