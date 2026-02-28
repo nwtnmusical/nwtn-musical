da80f9718128ad7f744848e212c7fd0fc66fd675
nwtnmusical <nwtnmusical@gmail.com>
2026-02-28 13:58:22 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#d12200',
+          dark: '#a51502',
+          light: '#f8c5c0',
+          secondary: '#cf2100',
+        }
+      },
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+        'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      }
+    },
+  },
+  plugins: [],
+}
